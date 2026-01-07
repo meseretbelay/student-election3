a21e022426bcda35a1906cf9a45e4300a1df9d77
Meseret Belay <meseretb@gmail.com>
2026-01-07 03:20:02 +0300

    Initial commit: student election project

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
   webpack: (config) => {
-    config.watchOptions = {
-      ignored: [
-        "**/C:/hiberfil.sys",
-        "**/C:/pagefile.sys",
-      ],
-    };
     return config;
   },
+
+  // Silences Turbopack warning
+  turbopack: {},
 };
 
 export default nextConfig;
